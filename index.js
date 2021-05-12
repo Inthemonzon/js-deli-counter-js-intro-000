d8d34952345250599ea0072a0bd63d313c4fbf8e
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-12 17:41:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeli){
 var line = []
 function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+ [i+1] + `.` + katzDeli[i])
+    line.push(` `+ [i+1] + `. ` + katzDeli[i])
   }
   if (katzDeli.length === 0) {
     return "The line is empty.";
